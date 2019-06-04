a5455f6dd65414e69c11e6a6514c600cd504468f
eneylton <eneylton@hotmail.com>
2019-06-04 18:30:00 -0300

    Exemplo file upload 01

---
+++ src/main/java/com/algaworks/contato/resource/FotosResource.java
@@ -0,0 +1,26 @@
+package com.algaworks.contato.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.contato.storage.Disco;
+
+@RestController
+@RequestMapping("/fotos")
+public class FotosResource {
+	
+	@Autowired
+	private Disco disco;
+	
+	@CrossOrigin
+	@PostMapping
+	public void upload(@RequestParam MultipartFile foto) {
+		disco.salvarFoto(foto);
+	}
+
+}
